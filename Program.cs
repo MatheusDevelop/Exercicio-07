73a3dd05ef9fe5cf8623f4ee9d15d7083a6d2389
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-06-02 16:44:44 -0300

    subindo arquivos

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrimeiroPoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem jogador1 = new Personagem();
+            jogador1.nome = "Barbosa lindo";
+            jogador1.idade = 18;
+
+            Console.WriteLine( jogador1.Correr(2405.5f) );
+            Console.WriteLine( "Força : " + jogador1.Atacar(600f) );
+            
+            jogador1.pulando = true;
+            Console.WriteLine( "Força : " + jogador1.Atacar(40f) );
+
+        }
+    }
+}
