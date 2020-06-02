cb9f9f63b7ea7ac7b37bdfb64a4492d1260cc98d
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-06-02 17:00:31 -0300

    subidno

--- Cartao.cs
+++ Cartao.cs
@@ -6,11 +6,11 @@ namespace Encapsulamento
         public string numero {get; set;}
         public string titular {get; set;}
         public string bandeira {get; set;}
-        public string token = "aa89s7d8989asd9798as7d98a7ds98a7sd89a7sd987asd98a7sd89";
+        public string token = "asdasd45454s5ad4545a8a7ds98a7sd89a7sd987asd98a7sd89";
         public string cvv {get; set;} 
 
         public void AprovarCompra(){
-            System.Console.WriteLine("Compra aprovada!");
+            System.Console.WriteLine("Aprovado!");
         }  
 
         private bool ValidarToken(){
--- MasterCard.cs
+++ MasterCard.cs
@@ -5,8 +5,8 @@ namespace Encapsulamento
         public int parcelas {get; set;}
 
         public void ComprarComDescontoMastercard(float desconto){
-            // Utilizando o encapsulamento "protected"
-            cvv = "456";
+            
+            cvv = "665";
 
             System.Console.WriteLine($"Compra realizada com {parcelas} parcelas e R$ {desconto} de desconto");
         }
